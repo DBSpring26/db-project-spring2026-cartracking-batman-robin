a592aa6d053a6b708329c62628369a708d88937d
Jesusrodz <jesus.rodriguez78@upr.edu>
2026-03-12 00:22:04 -0400

    Add ETL script

--- data_to_import/ETL/main.py
+++ data_to_import/ETL/main.py
@@ -1,12 +1,69 @@
 from pathlib import Path
 import pandas as pd
+from db import get_connection
 
-base_dir = Path(__file__).resolve().parent
-file_path = base_dir.parent / "trip.parquet"
 
-print(file_path)
+def import_trip(cursor):
+    base_dir = Path(__file__).resolve().parent
+    file_path = base_dir.parent / "trip.parquet"
 
-df = pd.read_parquet(file_path)
+    df = pd.read_parquet(file_path)
 
-print(df.columns.tolist())
-print(df.head())
+    print(file_path)
+    print(df.columns.tolist())
+    print(df.head())
+
+    query = """
+    INSERT INTO public.trip (
+        trip_id,
+        vehicle_id,
+        start_ts,
+        end_ts,
+        start_geom,
+        end_geom,
+        distance_km
+    )
+    VALUES (
+        %s,
+        %s,
+        %s,
+        %s,
+        ST_GeomFromEWKB(decode(%s, 'hex')),
+        ST_GeomFromEWKB(decode(%s, 'hex')),
+        %s
+    )
+    ON CONFLICT (trip_id) DO NOTHING;
+    """
+
+    for _, row in df.iterrows():
+        cursor.execute(query, (
+            row["trip_id"],
+            row["vehicle_id"],
+            row["start_ts"],
+            row["end_ts"],
+            row["start_geom"],
+            row["end_geom"],
+            row["distance_km"]
+        ))
+
+
+def main():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    try:
+        import_trip(cursor)
+        conn.commit()
+        print("Trip imported.")
+
+    except Exception as e:
+        conn.rollback()
+        print("Error:", e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+if __name__ == "__main__":
+    main()
