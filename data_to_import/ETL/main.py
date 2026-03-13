e1a8b10bd584646ef854a13f0773e3cc1a34e759
Jesusrodz <jesus.rodriguez78@upr.edu>
2026-03-12 22:41:47 -0400

    Parking area

--- data_to_import/ETL/main.py
+++ data_to_import/ETL/main.py
@@ -4,15 +4,10 @@ from db import get_connection
 
 
 def import_vehicle(cursor):
-
     base_dir = Path(__file__).resolve().parent
     file_path = base_dir.parent / "vehicle.parquet"
 
     df = pd.read_parquet(file_path)
-
-    print(file_path)
-    print(df.columns.tolist())
-    print(df.head())
 
     query = """
     INSERT INTO public.vehicle (
@@ -42,14 +37,47 @@ def import_vehicle(cursor):
         ))
 
 
+def import_parking_area(cursor):
+    base_dir = Path(__file__).resolve().parent
+    file_path = base_dir.parent / "parking_area.parquet"
+
+    df = pd.read_parquet(file_path)
+
+    query = """
+    INSERT INTO public.parking_area (
+        parking_area_id,
+        name,
+        capacity,
+        geom,
+        created_at
+    )
+    VALUES (%s,%s,%s,ST_GeomFromEWKB(decode(%s,'hex')),%s)
+    ON CONFLICT (parking_area_id) DO NOTHING;
+    """
+
+    for _, row in df.iterrows():
+        cursor.execute(query, (
+            row["parking_area_id"],
+            row["name"],
+            row["capacity"],
+            row["geom"],
+            row["created_at"]
+        ))
+
+
 def main():
     conn = get_connection()
     cursor = conn.cursor()
 
     try:
+        print("Importing vehicle...")
         import_vehicle(cursor)
+
+        print("Importing parking_area...")
+        import_parking_area(cursor)
+
         conn.commit()
-        print("Vehicle importate")
+        print("All Imported.")
 
     except Exception as e:
         conn.rollback()
