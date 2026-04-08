eca46182cb45597c2ca14942f00f8f57fa8a089e
Jesusrodz <jesus.rodriguez78@upr.edu>
2026-04-08 19:13:36 -0400

    fixed and backed/bd.py

--- data_to_import/ETL/db.py
+++ data_to_import/ETL/db.py
@@ -1,13 +1,12 @@
 import psycopg2
-from psycopg2.extras import RealDictCursor
 
 
 def get_connection():
-    return psycopg2.connect(
+    conn = psycopg2.connect(
         host="dpg-d6ovsu15pdvs739ofrn0-a.virginia-postgres.render.com",
         port=5432,
         dbname="declass",
         user="alexisagosto",
-        password="ehx0AjqfLrNdUErWtDRLgimnorzscTDd",
-        cursor_factory=RealDictCursor
+        password="ehx0AjqfLrNdUErWtDRLgimnorzscTDd"
     )
+    return conn
